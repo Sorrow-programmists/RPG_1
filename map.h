c7ef79be23d8c9a523888ade5bcd08722535b392
Migmas <kostyan37@mail.ru>
2016-04-20 20:05:40 +0300

    Чье это?
    аааа

--- map.h
+++ map.h
@@ -3,18 +3,21 @@
 #include <algorithm>
 #include <map>
 #include "TextureManager.h"
+#include "Object.h"
+#include <iostream>
 
 class Map {
 public:
 
-	Map(int width,int height) : _width(width), _height(height)
+	Map(int widthOfMap,int heightOfMap, int widthOfSprite, int heightOfSprite) 
+		: _width(widthOfMap), _height(heightOfMap), _widthS(widthOfSprite), _heightS(heightOfSprite)
 	{
-		_map = new uint8_t*[width];
-		for (int i(0); i < width; i++)
-			_map[i] = new uint8_t[height];
+		_map = new uint8_t*[_width];
+		for (int i(0); i < _width; i++)
+			_map[i] = new uint8_t[_height];
 	}
 
-	char getMapCode(int X,int Y)
+	const char &getMapCode(int X,int Y) const
 	{
 		return _map[X][Y];
 	}
@@ -31,40 +34,80 @@ public:
 				_map[i][g] = (rand()%3);
 		
 	}
-
-	void setTexture(sf::Image * image)
+	void setObject(const Object &obj, const unsigned int &index)
 	{
-		_image = TextureManager::addImage(*image);
+		_obj.insert(std::pair<int,Object>(index, obj));
+	}
+	const std::map<int, Object> &getStlMap() const
+	{
+		return _obj;
 	}
 
-	void loadTextureFromImage(const sf::IntRect &size)
+	const Object &getObject(const unsigned int &index) const
 	{
-		_fullTexture = TextureManager::loadTextureFromImage(*_image);
-		for (int i(0); i < _image->getSize().x; i += size.width)
+		return _obj.at(index);
+	}
+	/*
+	void setTexture(const sf::Image & image)
+	{
+		_image = image;
+	}
+	void loadSpriteFromImage(const sf::IntRect &size)
+	{
+		_fullTexture.loadFromImage(_image);
+		for (int i(0); i < _image.getSize().x; i += size.width)
 		{
-			for (int j(0); j < _image->getSize().y; j += size.height)
+			for (int j(0); j < _image.getSize().y; j += size.height)
 			{
 				sf::Sprite sprite;
-				sprite.setTexture(*_fullTexture);
+				sprite.setTexture(_fullTexture);
 				sprite.setTextureRect(sf::IntRect(size.left + i, size.top + j, size.width, size.height));
-				TextureManager::makeSprite();
-				_texture.insert(std::pair<int,sf::Sprite*>(_texture.size() + 1, &sprite));
+				_sprite.emplace_back(sprite);
 			}
 		}
+	}*/
+	std::vector<int> getMapIndexList()
+	{
+		std::vector<int> v;
+		for (int i(0); i < _width; i++)
+			for (int j(0); j < _height; j++)
+			{
+				auto f = std::find(v.begin(),v.end(),_map[i][j]);
+				if (f != v.end())
+				{
+					v.push_back(_map[i][j]);
+				}
+			}
+		std::sort(v.begin(), v.end());
+		return v;
 	}
-
+	void setSpriteSize(int width, int height)
+	{
+		_width = width;
+		_height = height;
+	}
 	void drawMap(sf::RenderWindow &window, const sf::IntRect &area)
 	{
-		if (!_texture.empty())
+		if (!_obj.empty())
 		{
 			
-			for (int i(area.left); i < (area.left + area.width) / _texture.at(0)->getLocalBounds().width; i++)
-				for (int j(area.top); j < (area.top + area.height) / _texture.at(0)->getLocalBounds().height; j++)
+			for (int i(area.left); i < (area.left + area.width); i+= _widthS)
+				for (int j(area.top); j < (area.top + area.height); j+= _heightS)
 				{
-					window.draw(*_texture.at(_map[i][j]));
+					sf::Sprite sprite(_obj.at(_map[i][j]).getSprite());
+					sprite.setTextureRect(sf::IntRect(i, j, _widthS, _heightS));
+					window.draw(sprite);
 				}
 			return;
 		}
+	}
+	const sf::Vector2u &getSize() const
+	{
+		return sf::Vector2u(_width, _height);
+	}
+	const sf::Vector2u &getSpriteSize() const
+	{
+		return sf::Vector2u(_widthS, _heightS);
 	}
 
 	~Map()
@@ -73,15 +116,54 @@ public:
 			delete[] _map[i];
 		delete[] _map;
 	}
+	
 
+	Map &operator=(const Map &right)
+	{
+		return Map(right);
+	}
+
+	Map(const Map &right)
+	{
+		_height = right.getSize().y;
+		_width = right.getSize().x;
+
+		_widthS = right.getSpriteSize().x;
+		_heightS = right.getSpriteSize().y;
+
+
+		_map = new uint8_t*[_width];
+		for (int i(0); i < _width; i++)
+			_map[i] = new uint8_t[_height];
+
+		for (int i(0); i < _width; i++)
+			for (int j(0); j < _height; j++)
+				_map[i][j] = right.getMapCode(i, j);
+		_obj = right.getStlMap();
+	}
+	
 private:
-	int _width, _height;
+	int _widthS, _heightS;
 
 	uint8_t **_map;
 
-	std::map<int, sf::Sprite*> _texture;
+	std::map<int,Object> _obj;
 
-	sf::Image *_image;
+	int _width, _height;
+	/*
+	sf::Image _image;
 
-	sf::Texture *_fullTexture;
+	sf::Texture _fullTexture;
+	
+	
+	template<class _lnlt,class Ty>
+	_lnlt _findSorted(_lnlt begin, _lnlt end, Ty && val)
+	{
+		if begin
+		_lnlt t = (begin + end) / 2;
+		
+	}*/
 };
+
+
+
